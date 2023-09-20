61567f82da979299db12924df87114b4a4b0a8f6
shuoli90 <lishuo1@seas.upenn.edu>
2023-09-20 15:30:58 -0400

    update opensource

--- TRAC/opensource.py
+++ TRAC/opensource.py
@@ -2,25 +2,33 @@ import transformers
 from transformers import AutoTokenizer
 import torch
 
-model = 'daryl149/llama-2-7b-chat-hf'
 
-tokenizer = AutoTokenizer.from_pretrained(model)
+def setup_openmodel(model='daryl149/llama-2-7b-chat-hf'):
+    model = 'daryl149/llama-2-7b-chat-hf'
 
-pipeline = transformers.pipeline(
-    'text-generation',
-    model=model,
-    torch_dtype=torch.float16,
-    device_map='auto',
-)
+    tokenizer = AutoTokenizer.from_pretrained(model)
 
-sequences = pipeline(
-    'I liked “Breaking Bad” and “Band of Brothers”. Do you have any recommendations of other shows I might like?\n',
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-    max_length=200,
-)
+    pipeline = transformers.pipeline(
+        'text-generation',
+        model=model,
+        torch_dtype=torch.float16,
+        device_map='auto',
+    )
+    return model, pipeline, tokenizer
 
-for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
+
+def ask_openmodel(prompt, pipeline, tokenizer, return_sequences=10):
+
+    sequences = pipeline(
+        prompt,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=return_sequences,
+        eos_token_id=tokenizer.eos_token_id,
+        max_length=400,
+    )
+    return sequences
+
+def delete_model(model):
+    del model
+    torch.cuda.empty_cache()
