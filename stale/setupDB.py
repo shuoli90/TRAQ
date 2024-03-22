9e6c88a7591c089c64ce1d9e1a82cf8ecc4ff9c5
shuoli90 <lishuo1@seas.upenn.edu>
2024-03-22 14:42:55 -0400

    remove stale

--- stale/setupDB.py
+++
@@ -1,9 +0,0 @@
-import json
-from pymongo import MongoClient
-from datasets import load_dataset
-
-
-myclient = MongoClient("localhost")
-mydb = myclient["kilt"]
-mycol = mydb["knowledgesource"]
-breakpoint()
